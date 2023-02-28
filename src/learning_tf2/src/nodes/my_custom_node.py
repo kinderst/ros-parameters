5d070d30378dc5681dfcd0fd544cb7b2f9d872a8
Scott Kinder <kinders@uw.edu>
2023-02-28 03:00:38 -0700

    fixed custom node name

--- src/learning_tf2/src/nodes/my_custom_node.py
+++ src/learning_tf2/src/nodes/my_custom_node.py
@@ -3,7 +3,7 @@ import rospy
 
 
 if __name__ == '__main__':
-    rospy.init_node('tf2_turtle_broadcaster')
+    rospy.init_node('my_custom_node_name')
     custom_message = rospy.get_param('~mymsg')
     rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~mymsg'), custom_message)
     # print
